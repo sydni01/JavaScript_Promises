9c3ac377a721e85b382a29d7c868d6d8be8ff513
_Sydni Miles_ <_[sydnibean14@gmail.com](mailto:sydnibean14@gmail.com)_>
2022-06-27 12:03:34 -0400

    Updates to JS Promises

--- app.js
+++ app.js
@@ -2,3 +2,37 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function watchTurorialCallback(callback, errorCallback) {
+    let userLeft = false;
+    let userWatchingLiveStream = true;
+  
+    if (userLeft) {
+      errorCallback({
+        name: "User Left",
+        message: ":(",
+      });
+    } else if (userWatchingLiveStream) {
+      callback("Thumbs up and Subscribe");
+    }
+  }
+  
+  watchTurorialCallback(
+    (message) => {
+      console.log(message);
+    },
+    (error) => {
+      console.log(error.name + " " + error.message);
+    }
+  );
+
+  let promise = new Promise(() => {
+      let userLeft = Boolean(Math.round(Math.random()));
+
+      if (userLeft) {
+          PromiseRejectionEvent("User left.");
+       } else {
+              resolve("Thumbs up and subscribe!");
+          }
+      }
+  )   
